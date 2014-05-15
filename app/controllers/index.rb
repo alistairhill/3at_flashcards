3d43f2cedff866d625c31ce52ade79e3e9b1d0fc
Ali & Alejandro <pair+alistairhill.alosada@devbootcamp.com>
2014-05-15 12:14:00 -0700

    migration and model done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,25 +3,8 @@ get '/' do
   erb :index
 end
 
-get '/bands' do
-  @band_names = Band.all.map(&:name)
-  erb :bands
-end
-
-post '/bands' do
-  new_band = Band.create!(name: params[:name])
-  redirect "/bands/#{new_band.id}"
-end
-
-get '/bands/new' do
-  erb :new_band
-end
-
-get '/bands/:id' do
-  @band = Band.find(params[:id])
-  erb :show_band
-end
-
-get '/info' do
-  Demo.new(self).info
+get '/decks' do
+  session={user_id: 404, user_name: "Rao"}
+  "#{session}"
+  #erb :decks
 end
