250cb68267bd6bfaff2a753151b16f5e53ec6578
Amy Foster & Alejandro <pair+akfoster.alosada@devbootcamp.com>
2014-05-16 16:21:05 -0700

    fixed description

--- spec/games_controller_spec.rb
+++ spec/games_controller_spec.rb
@@ -7,18 +7,21 @@ describe "Games controller" do
   describe "POST /games/:game_id" do
     context "When a correct answer is entered" do
 
-      it 'updates the score by 2' do
+      it 'updates the score by 5' do
 
-        game = Game.create(user_id: 1, deck_id: 1)
-        game_id = game.id
-        card = Card.create(question: "spec test", answer: "true", deck_id: 1)
+
         deck = Deck.create(name: "spectest", description: "testing the feature")
+        card = Card.create(question: "spec test", answer: "true", deck_id: deck.id)
+        game = Game.create(user_id: 1, deck_id: deck.id)
+        p game
+        game_id = game.id
         old_score = game.score
-        fake_session = { :game_id => game.id }
-        post '/games', {guess: "true"}, { 'rack.session' => fake_session }
+        fake_session = { :game_id => game.id, :game_deck => [deck]}
+        post '/turn', {guess: "true"}, { 'rack.session' => fake_session }
         game.reload
+        p game
         new_score = game.score
-        expect(game.score - old_score).to eq(2)
+        expect(new_score - old_score).to eq(5)
 
       end
 
